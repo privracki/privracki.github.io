5139df2052ddb3525db397dfd1a19c261bb9f2da
Jano Privracki <jano.privracki@protonmail.com>
2020-11-22 12:06:29 +0100

    Initial commit

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,17 +1,15 @@
-function toggle(class);
-
 let hidden = 1, // All image elements are hidden by default.
 	allImages = document.getElementsByTagName('img');
 
 function toggle(class) {
 	let imagesByClass = document.getElementsByClassName(class);
 
-	if (hidden == 0) {
-		for (let i = 0; i < images.length; ++i)
+	if (hidden === 0) {
+		for (let i = 0; i < allImages.length; ++i)
 			allImages[i].style.display = "none";
 		hidden = 1;
 	}
-	for (let i = 0; i < images.length; ++i)
+	for (let i = 0; i < imagesByClass.length; ++i)
 		imagesByClass[i].style.display = "block";
 	hidden = 0;
 }
